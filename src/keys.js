058c22cdffacd0127ed6817dbe24d6843bf53f99
saidnahum <70162272+saidnahum@users.noreply.github.com>
2021-09-24 21:20:29 -0500

    Initial commit

---
+++ src/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+   database: {
+      URI: 'mongodb://localhost/imgshare',
+      URI_ATLAS: 'mongodb+srv://said:890318hvz@mongodb-server.vygtk.mongodb.net/imgshare?retryWrites=true&w=majority'
+   }
+}
